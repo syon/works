cdbb6fe07f4017854a71953cdca2a66300aadb37
syon <syon.xiii@gmail.com>
2018-02-14 00:28:41 +0900

    :lipstick: author layout

--- components/AppLogo.vue
+++ components/AppLogo.vue
@@ -1,51 +1,57 @@
 <template lang="pug">
 .author
-  .author-left
-    img.avatar(src="/syon.jpg")
-  .author-right
-    .author-name syon
-    .author-attr
-      i.
-        <svg width="16" height="16" class="octicon octicon-location" viewBox="0 0 12 16" version="1.1" aria-hidden="true"><path fill-rule="evenodd" d="M6 0C2.69 0 0 2.5 0 5.5 0 10.02 6 16 6 16s6-5.98 6-10.5C12 2.5 9.31 0 6 0zm0 14.55C4.14 12.52 1 8.44 1 5.5 1 3.02 3.25 1 6 1c1.34 0 2.61.48 3.56 1.36.92.86 1.44 1.97 1.44 3.14 0 2.94-3.14 7.02-5 9.05zM8 5.5c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"></path></svg>
-      span Tokyo, Japan
-    .links
-      a.link-item(href="https://github.com/syon" target="_blank")
-        i.
-          <svg width="16" height="16" class="octicon octicon-mark-github" viewBox="0 0 16 16" version="1.1" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>
-        span GitHub
+  .author-container
+    .author-avatar
+      img.avatar(src="/syon.jpg")
+    .author-title
+      .namerepo
+        .author-name syon
+        .slash /
+        .repo works
+      .github-link
+        a.anchor(href="https://github.com/syon" target="_blank")
+          i.
+            <svg width="16" height="16" class="octicon octicon-mark-github" viewBox="0 0 16 16" version="1.1" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>
+          span GitHub
 </template>
 
 <style lang="stylus" scoped>
 .author
   display flex
+  justify-content space-between
 
-.author-left
-  margin-right 1rem
+.author-container
+  display flex
+  align-items flex-start
 
 .avatar
   width 72px
   height 72px
+  margin-right 1rem
   border-radius 50%
 
-.author-name
-  font-size 2rem
-  height 72px
+.author-title
   display flex
-  align-items center
+  align-items flex-start
+  flex-direction column
 
-.author-attr
+  .namerepo
+    display flex
+    align-items center
+    font-size 2rem
+    line-height 2
+    .slash
+      padding 0 0.2em
+      font-weight 200
+
+.anchor
   display flex
   align-items center
-  margin 1em 0
+  font-size 0.8rem
+  color inherit
   i
     display flex
     margin-right 0.5em
-
-.link-item
-  display flex
-  align-items center
-  margin 1em 0
-  i
-    display flex
-    margin-right 0.5em
+  span
+    padding-top 2px
 </style>
